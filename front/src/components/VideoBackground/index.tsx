8bab080f07ccdeb0ab95f2119ff916b838d27712
ilya.nosenko <ilya.nosenko@karuna.group>
2025-02-02 01:07:59 +0300

    added button go to main page

--- front/src/components/VideoBackground/index.tsx
+++ front/src/components/VideoBackground/index.tsx
@@ -1,34 +1,42 @@
 import { useRef, useState } from "react";
 
 function VideoBackground() {
-    const videoRef = useRef<HTMLVideoElement>(null);
-    const [isMuted, setIsMuted] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isMuted, setIsMuted] = useState(true);
 
-    const toggleSound = () => {
-        if (videoRef.current) {
-            videoRef.current.muted = !videoRef.current.muted;
-            setIsMuted(!isMuted);
+  const toggleSound = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !videoRef.current.muted;
+      setIsMuted(!isMuted);
 
-            if (!videoRef.current.muted) {
-                videoRef.current.play().catch((e) => console.error("Ошибка воспроизведения:", e));
-            }
-        }
-    };
+      if (!videoRef.current.muted) {
+        videoRef.current
+          .play()
+          .catch((e) => console.error("Ошибка воспроизведения:", e));
+      }
+    }
+  };
 
-    return (
-        <div className="absolute top-0 left-0 w-full h-full">
-            <video ref={videoRef} autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover">
-                <source src="/background.mp4" type="video/mp4"/>
-            </video>
+  return (
+    <div className="absolute top-0 left-0 w-full h-full">
+      <video
+        ref={videoRef}
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/background.mp4" type="video/mp4" />
+      </video>
 
-            <button
-                onClick={toggleSound}
-                className="absolute bottom-5 right-5 p-3 bg-black bg-opacity-50 text-white rounded-full"
-            >
-                {isMuted ? "🔇 Включить звук" : "🔊 Выключить звук"}
-            </button>
-        </div>
-    );
+      <button
+        onClick={toggleSound}
+        className="absolute bottom-5 right-5 p-3 bg-black bg-opacity-50 opacity-80 text-white rounded-full"
+      >
+        {isMuted ? "🔇 Включить звук" : "🔊 Выключить звук"}
+      </button>
+    </div>
+  );
 }
 
 export default VideoBackground;
--- front/src/pages/GamePage.tsx
+++ front/src/pages/GamePage.tsx
@@ -1,16 +1,22 @@
-import {useParams} from "react-router";
+import { Link, useParams } from "react-router";
 import Questions from "../components/Questions";
 import PageHeader from "../components/PageHeader";
 
 function GamePage() {
-    const {topic} = useParams<{ topic: string }>();
+  const { topic } = useParams<{ topic: string }>();
 
-    return <div className='page'>
-        <PageHeader title={topic || ''}/>
-        {
-            topic ? <Questions topics={[topic]}/> : <div>Empty</div>
-        }
+  return (
+    <div className="page">
+      <PageHeader title={topic || ""} />
+      {topic ? <Questions topics={[topic]} /> : <div>Empty</div>}
+      <Link
+        to={"/"}
+        className="absolute bottom-5 right-5 p-3 bg-stone-900 bg-opacity-80 text-rose-200 rounded-xl font-medium"
+      >
+        Go to menu
+      </Link>
     </div>
+  );
 }
 
 export default GamePage;
