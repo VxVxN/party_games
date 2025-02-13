bc6f53294bc045e1229683ef500da9beaac59bef
Vladimir <vxvxn@yandex.ru>
2025-02-13 21:10:10 +0300

    add log middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,19 +1,37 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 type Server struct {
 	fileDataByName map[string][]byte
+	logger         zerolog.Logger
 }
 
 func NewServer() *Server {
 	return &Server{
 		fileDataByName: make(map[string][]byte),
+		logger:         log.Logger,
 	}
 }
 
 func (server *Server) ListenAndServe(handler http.Handler) error {
-	return http.ListenAndServe(":8080", handler)
+	port := 8080
+	server.logger.Info().Int("port", port).Msg("Starting server")
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
+}
+
+func (server *Server) LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		server.logger.Info().Str("method", r.Method).Str("path", r.URL.Path).Msg("Received request")
+		next.ServeHTTP(w, r)
+		server.logger.Info().Str("method", r.Method).Str("path", r.URL.Path).Dur("duration", time.Now().Sub(startTime)).Msg("Completed request")
+	})
 }
