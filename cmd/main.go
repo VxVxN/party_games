ed0df6659c5b7d0fe0983a0632484ab7baa677a0
Vladimir <vxvxn@yandex.ru>
2025-02-21 00:19:32 +0300

    add /neverhaveiever to routes

--- cmd/main.go
+++ cmd/main.go
@@ -11,9 +11,9 @@ func main() {
 	server := server.NewServer()
 
 	router := http.NewServeMux()
-	router.Handle("GET /description", server.LogMiddleware(server.INeverController.DescriptionHandler))
-	router.Handle("GET /topic/list", server.LogMiddleware(server.INeverController.TopicListHandler))
-	router.Handle("POST /topic/records", server.LogMiddleware(server.INeverController.TopicRecordsHandler))
+	router.Handle("GET /neverhaveiever/description", server.LogMiddleware(server.INeverController.DescriptionHandler))
+	router.Handle("GET /neverhaveiever/topic/list", server.LogMiddleware(server.INeverController.TopicListHandler))
+	router.Handle("POST /neverhaveiever/topic/records", server.LogMiddleware(server.INeverController.TopicRecordsHandler))
 
 	if err := server.ListenAndServe(router); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
